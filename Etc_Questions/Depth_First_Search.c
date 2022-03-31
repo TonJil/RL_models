684c6b5553e3bb3bc4e8da565ca587a293f67404
TonJil <109621911+TonJil@users.noreply.github.com>
2022-03-31 20:42:39 +0000

    submod update

--- Etc_Questions/Depth_First_Search.c
+++ Etc_Questions/Depth_First_Search.c
@@ -39,4 +39,4 @@ void Depth_First_Search(int x, int y, int length)
 	
 	// 오른쪽으로 이동할 수 있다면 이동!
 	if (x < n - 1 && map[y][x + 1] != 0) 
-		Depth_First_Search(x + 1, y, length + 1
+		Depth_First_Search(x + 1, y, length + 1);
