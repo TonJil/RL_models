09fecfa81dc41a572fa03fd7271401abbbd9d6b1
TonJil <109621911+TonJil@users.noreply.github.com>
2023-01-15 22:06:56 +0000

    test theme

--- Etc_Questions/anagram.c
+++ Etc_Questions/anagram.c
@@ -1,3 +1,9 @@
 
 
 // Anagram 이란 단어 철자의 순서가 바뀌어서 다른 단어가 될 때를 칭하는 용어이다.
+// 즉, apple 과 ppale 는 서로 아나그램인 것 이다.
+// 단순하게는 'triangle = integral', 'earth = heart' 등의 한 단어로 이루어진 아나그램부터 
+// 'George Bush = He bugs Gore(부시와 앨 고어의 관계를 풍자한 아나그램)'  문자열 아나그램
+
+
+// 아나그램 체크 프로그램
