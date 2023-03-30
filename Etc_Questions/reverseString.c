a9d8d845a488fbc6295345c742c863eaa2bb25d6
TonJil <109621911+TonJil@users.noreply.github.com>
2023-03-30 06:48:43 +0000

    patching tests

--- Etc_Questions/reverseString.c
+++ Etc_Questions/reverseString.c
@@ -4,3 +4,8 @@
 // 모든 단어은 스페이스로 구분. 문장 부호는 글자로 취급.
 void reverseString( char str[], int start, int end )
 {
+   char temp;
+   while( end > start ) {
+      temp = str[start];
+      str[start] = str[end];
+      str[end] = temp;
