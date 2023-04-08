884c24e0b89b27ec38665cb0e0afc306cc599258
TonJil <109621911+TonJil@users.noreply.github.com>
2023-04-08 16:28:53 +0000

    ASoC: wm8994: correct BCLK DIV 348 to 384

--- Etc_Questions/strcmp.c
+++ Etc_Questions/strcmp.c
@@ -4,3 +4,7 @@
 // 첫 번째 인자가 더 크면 1 반환
 // 두 번째 인자가 더 크면 -1 반환
 int my_strcmp(const char * pStr1, const char * pStr2){
+     if(pStr1 == NULL || pStr2 == NULL) 
+     	return -1;
+ 
+     for( ; (*pStr1) != '\0' || (*pStr2) != '\0'; pStr1++, pStr2++){
