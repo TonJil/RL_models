a26db1f4cbc1f9533fe52b3e043c42c90fdff333
TonJil <109621911+TonJil@users.noreply.github.com>
2023-04-13 09:51:28 +0000

    Finish readme

--- MathAlgorithm/Factorial/Questions/factorial_tail_rec.c
+++ MathAlgorithm/Factorial/Questions/factorial_tail_rec.c
@@ -5,3 +5,9 @@
 
 unsigned int factorial(unsigned int n){
     return factorial_tail_rec(n, 1);
+}
+ 
+unsigned int factorial_tail_rec(unsigned int n, unsigned int acc){
+
+    if( n == 1 ) return acc;
+    return factorial_tail_rec(n-1, acc * n);
