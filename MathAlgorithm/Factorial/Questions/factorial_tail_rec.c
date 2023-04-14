36a0541f995b30317735e5ce37b656e97c0ac211
TonJil <109621911+TonJil@users.noreply.github.com>
2023-04-14 05:31:00 +0000

    Updated .travis.yml.

--- MathAlgorithm/Factorial/Questions/factorial_tail_rec.c
+++ MathAlgorithm/Factorial/Questions/factorial_tail_rec.c
@@ -13,3 +13,11 @@ unsigned int factorial_tail_rec(unsigned int n, unsigned int acc){
     return factorial_tail_rec(n-1, acc * n);
 }
 
+
+//========================================================
+//일반적인 factorial
+unsigned int factorial(unsigned int n){
+
+	if (n == 0){
+		return 1;
+	}
